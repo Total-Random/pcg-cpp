2e7b2145eb20efd6a97bf1094d02fb51736f30d0
AkiKuriChan <akikurichan@gmail.com>
2026-03-22 19:28:14 +0700

    Cleanup: Remove build-test and pcg-next-master from git tracking.

--- build-test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Cpp/project/pcg-cpp
-# Build directory: D:/Cpp/project/pcg-cpp/build-test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("sample")
-subdirs("test-high")
--- pcg-next-master/sample/basic_usage.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "pcg-next/random.hpp"
-
-int main()
-{
-    pcg32 rng(42);
-    return rng() + rng();
-}
